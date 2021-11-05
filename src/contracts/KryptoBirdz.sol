db6b6ab83ba3fc6950088a680352ab5e9ddeae97
ibtehaz <ibtehaz.utsay@gmail.com>
2021-11-05 03:48:17 -0700

    first commit

---
+++ src/contracts/KryptoBirdz.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract Kryptobird {
+    string public symbol;
+    string public name;
+
+    constructor() {
+        name="Kryptobird";
+        symbol="KBIRDZ";
+    }
+}
